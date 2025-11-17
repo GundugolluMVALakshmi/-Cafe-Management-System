fa8a7b03807dd40bd10cfbcc7a9a1bbe5b3650b0
GundugolluMVALakshmi <140155668+GundugolluMVALakshmi@users.noreply.github.com>
2025-11-17 12:22:23 +0530

    Update report.py

--- report.py
+++ report.py
@@ -1,13 +1,15 @@
+# report.py
 from db_config import get_db_connection
 
 def sales_report():
     conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute("SELECT * FROM orders")
-    rows = cursor.fetchall()
-    total_sales = sum(row[3] for row in rows)
-    print("Sales Report:")
-    for row in rows:
-        print(row)
-    print(f"Total Sales: ₹{total_sales}\n")
+    cur = conn.cursor()
+    cur.execute("SELECT order_id, item_name, quantity, total_price, order_time FROM orders ORDER BY order_time DESC")
+    rows = cur.fetchall()
     conn.close()
+    print("\nSales Report:")
+    total_sales = 0
+    for r in rows:
+        print(f"Order {r['order_id']} | {r['item_name']} x{r['quantity']} = ₹{r['total_price']} | {r['order_time']}")
+        total_sales += r['total_price']
+    print(f"\nTotal Sales: ₹{total_sales}\n")
